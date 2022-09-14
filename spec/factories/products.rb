98b3ac71eda25a34797bf10db947eea5f46eb1ff
Matheus Silva <matheussilva.fga@gmail.com>
2022-09-14 15:51:12 -0300

    Creating model and tests for Products

---
+++ spec/factories/products.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :product do
+    sequence(:name) { |n| "Product #{n}" }
+    description { Faker::Lorem.paragraph }
+    price { Faker::Commerce.price(range: 100.00..40.00) }
+    productable { nil }
+  end
+end
