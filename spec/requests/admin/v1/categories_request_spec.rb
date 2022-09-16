05d652bb9ea3ad80ae6d0525806eedc80d54b3af
Matheus Silva <matheussilva.fga@gmail.com>
2022-09-16 08:27:15 -0300

    Creating destroy request tests for Category

--- spec/requests/admin/v1/categories_request_spec.rb
+++ spec/requests/admin/v1/categories_request_spec.rb
@@ -24,13 +24,13 @@ RSpec.describe "Admin::V1::Categories", type: :request do
     context "with valid params" do
       let(:category_params) { { category: attributes_for(:category) }.to_json }
 
-      it 'Adds a new Category' do
+      it "Adds a new Category" do
         expect do
           post url, headers: auth_header(user), params: category_params
         end.to change(Category, :count).by(1)
       end
 
-      it 'returns last added Category' do
+      it "returns last added Category" do
         post url, headers: auth_header(user), params: category_params
         expect_category = Category.last.as_json(only: %i(id name))
         expect(body_json['category']).to eq(expect_category)
@@ -47,18 +47,18 @@ RSpec.describe "Admin::V1::Categories", type: :request do
         { category: attributes_for(:category, name: nil) }.to_json
       end
 
-      it 'does not add a new Category' do
+      it "does not add a new Category" do
         expect do
           post url, headers: auth_header(user), params: category_invalid_params
         end.to_not change(Category, :count)
       end
 
-      it 'returns error message' do
+      it "returns error message" do
         post url, headers: auth_header(user), params: category_invalid_params
         expect(body_json['errors']['fields']).to have_key('name')
       end
 
-      it 'returns unprocessable_entity status' do
+      it "returns unprocessable_entity status" do
         post url, headers: auth_header(user), params: category_invalid_params
         expect(response).to have_http_status(:unprocessable_entity)
       end
@@ -104,15 +104,43 @@ RSpec.describe "Admin::V1::Categories", type: :request do
         expect(category.name).to eq(old_name)
       end
 
-      it 'returns error message' do
+      it "returns error message" do
         patch url, headers: auth_header(user), params: category_invalid_params
         expect(body_json['errors']['fields']).to have_key('name')
       end
 
-      it 'returns unprocessable_entity status' do
+      it "returns unprocessable_entity status" do
         patch url, headers: auth_header(user), params: category_invalid_params
         expect(response).to have_http_status(:unprocessable_entity)
       end
+    end
+  end
+
+  context "DELETE /categories/:id" do
+    let!(:category) { create(:category) }
+    let(:url) { "/admin/v1/categories/#{category.id}" }
+
+    it "removes Category" do
+      expect do
+        delete url, headers: auth_header(user)
+      end.to change(Category, :count).by(-1)
+    end
+    
+    it "does not return any body content" do
+      delete url, headers: auth_header(user)
+      expect(body_json).to_not be_present
+    end
+
+    it "returns success status" do
+      delete url, headers: auth_header(user)
+      expect(response).to have_http_status(:no_content)
+    end
+
+    it "removes all associated product categories" do
+      product_categories = create_list(:product_category, 3, category: category)
+      delete url, headers: auth_header(user)
+      expected_product_categories = ProductCategory.where(id: product_categories.map(&:id))
+      expect(expected_product_categories).to eq(0)
     end
   end
 end
