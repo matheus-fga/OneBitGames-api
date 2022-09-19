234406bb8be4d3825089ca562ae3164464c117ba
Matheus Silva <matheussilva.fga@gmail.com>
2022-09-19 18:51:05 -0300

    Creating get request tests for Users

---
+++ spec/requests/admin/v1/users/admin_requests_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe "Admin::V1::Users as :admin", type: :request do
+  let(:logged_user) { create(:user) }
+
+  context "GET /users" do
+    let(:url) { "/admin/v1/users" }
+    let!(:users) { create_list(:user, 5, profile: :client) }
+
+    it "should return all users" do
+      get url, headers: auth_header(logged_user)
+      expect(body_json['users']).to contain_exactly *users.as_json(
+        only: %i(id name email profile)
+      )
+    end
+
+    it "should return success status" do
+      get url, headers: auth_header(logged_user)
+      expect(response).to have_http_status(:ok)
+    end
+  end
+end
