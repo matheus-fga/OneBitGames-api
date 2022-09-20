d35b00f6918e0ca166e742b9b87a94ed8d2f353d
Matheus Silva <matheussilva.fga@gmail.com>
2022-09-20 11:16:59 -0300

    Creating patch request tests for Users

--- spec/requests/admin/v1/users/admin_requests_spec.rb
+++ spec/requests/admin/v1/users/admin_requests_spec.rb
@@ -69,4 +69,59 @@ RSpec.describe "Admin::V1::Users as :admin", type: :request do
       end
     end
   end
+
+  context "PATCH /users/:id" do
+    let(:user) { create(:user) }
+    let(:url) { "/admin/v1/users/#{user.id}" }
+
+    context "with valid params" do
+      let(:new_name) { 'My New name' }
+      let(:user_params) do 
+        { user: { name: new_name } }.to_json
+      end
+
+      it "should update the user" do
+        patch url, headers: auth_header(logged_user), params: user_params
+        user.reload
+        expect(user.name).to eq new_name
+      end
+
+      it "should return the updated user" do
+        patch url, headers: auth_header(logged_user), params: user_params
+        user.reload
+        expected_user = user.as_json(
+          only: %i(id name code status discount_value max_use due_date)
+        )
+        expect(body_json['user']).to eq expected_user
+      end
+
+      it "should return success status" do
+        patch url, headers: auth_header(logged_user), params: user_params
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context "with invalid params" do
+      let(:invalid_user_params) do
+        { user: attributes_for(:user, name: nil) }.to_json
+      end
+
+      it "should not updates the user" do
+        old_name = user.name
+        patch url, headers: auth_header(logged_user), params: invalid_user_params
+        user.reload
+        expect(user.name).to eq old_name
+      end
+
+      it "should return an error message" do
+        patch url, headers: auth_header(logged_user), params: invalid_user_params
+        expect(body_json['errors']['fields']).to have_key('name')
+      end
+
+      it "should return unprocessable_entity status" do
+        patch url, headers: auth_header(logged_user), params: invalid_user_params
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+  end
 end
